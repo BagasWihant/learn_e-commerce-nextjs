a02863f2810fcfaa40d1f05924d5210d5e11ecd9
bagaswihant <bagaswihant@gmail.com>
2024-06-30 18:02:21 +0700

    change font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Lato } from "next/font/google";
+import { Inter } from "next/font/google";
 
-const lato = Lato({
+const font = Inter({
   subsets: ["latin"],
   weight: ["100", "300", "400", "700", "900"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
-    <div className={lato.className}>
-      <Component {...pageProps} />;
+  return (
+    <div className={font.className}>
+      <Component {...pageProps} />
     </div>
-  ) 
+  );
 }
